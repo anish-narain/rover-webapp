ce09cba9e2be420a81bac6b93bba4b4d7291ec23
anish-narain <69715492+anish-narain@users.noreply.github.com>
2023-05-26 11:37:53 +0100

    deployed on git pages!!!

---
+++ build/precache-manifest.31d5accb0479d5407fa68c8f85f3e280.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5909dd48bdd0ad9c0f1d",
+    "url": "/rover-webapp/static/css/main.7af59d35.chunk.css"
+  },
+  {
+    "revision": "5909dd48bdd0ad9c0f1d",
+    "url": "/rover-webapp/static/js/main.5909dd48.chunk.js"
+  },
+  {
+    "revision": "f317839bf06338803c56",
+    "url": "/rover-webapp/static/js/1.f317839b.chunk.js"
+  },
+  {
+    "revision": "15d77b8fa0da709b3b74",
+    "url": "/rover-webapp/static/js/runtime~main.15d77b8f.js"
+  },
+  {
+    "revision": "783a8fd006a050b34bc214ae18501aa4",
+    "url": "/rover-webapp/index.html"
+  }
+];
---
+++ build/service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/rover-webapp/precache-manifest.31d5accb0479d5407fa68c8f85f3e280.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/rover-webapp/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});

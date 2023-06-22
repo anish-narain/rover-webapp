1127354626df07945bb36a3885b9f57062bef436
anish-narain <69715492+anish-narain@users.noreply.github.com>
2023-06-22 16:01:53 +0100

    added matlab shortest path

--- src/App.js
+++ src/App.js
@@ -132,7 +132,7 @@ function App() {
       const url = URL.createObjectURL(blob);
       const link = document.createElement('a');
       link.href = url;
-      link.download = 'coordinates.txt';
+      link.download = 'output.txt';
       link.click();
     }
   };
